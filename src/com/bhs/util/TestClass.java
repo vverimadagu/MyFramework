2ce009ff55fec0b1273d1d03a7e2ce41ebd11772
vverimadugu <vvenkataramanareddys@gmail.com>
2017-03-03 23:17:39 +0530

    commit from remote
    commit from remote

--- src/com/bhs/util/TestClass.java
+++ src/com/bhs/util/TestClass.java
@@ -3,7 +3,7 @@ package com.bhs.util;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("In AAAAAAAAAAAAAAAAAAAAcMain");
+		System.out.println("In AAAAAAAAAAAAAAAAAAAAcBBBBBBBBBBBBBBBBBBBBBBBBBBBBBMain");
 
 	}
 
